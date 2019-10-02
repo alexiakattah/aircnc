8468a4e58cb6ccd5ac0abf57d9423295effe97b5
Alexia Kattah <alexiakattah_97@hotmail.com>
2019-10-02 15:03:16 -0300

    adicionando o frontend

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+
+export default function Dashboard() {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem("user");
+      const response = await api.get("/dashboard", {
+        headers: { user_id }
+      });
+      setSpots(response.data)
+    }
+    loadSpots();
+  }, []); //array vazio, vai executar somente uma vez, [filter]
+
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header />
+            <strong>{spot.company}</strong>
+            <span>{spot.price}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import api from '../../services/api';
+
+
+export default function Login({ history }){
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault(); //nao redireciona a pagina, permanece na mesma, mesmo depois do clique 
+    const response = await api.post('/sessions', { email })
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id); //armazena o id na memoria do navegador 
+
+    history.push('/dashboard'); //navegação para a proxima pagina 
+  }
+
+  function handleEmailChange(event){
+    setEmail(event.target.value);
+  }
+
+  return (
+    <>
+    <p>
+    Ofereça <strong>spots</strong>  para programadores e encontre  <strong>talentos</strong>  para sua empresa
+    </p>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="email">Email *</label>
+    <input 
+    type="email" 
+    id="email" 
+    placeholder="Seu melhor email"
+    value={email}
+    onChange={ handleEmailChange }
+    
+    />
+
+    <button className="btn" type="submit">Entrar</button>
+  </form>
+  </>
+
+  );
+}
