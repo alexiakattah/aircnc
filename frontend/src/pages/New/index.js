5058dafe0b82f7131a4f17f7bb71534e768eca26
Alexia Kattah <alexiakattah_97@hotmail.com>
2019-10-02 16:52:23 -0300

    styles in pages

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,6 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './styles.css'
+
 
 export default function New(){
-  return <div />
+  const [company, setCompany] = useState('');
+  const [techs, setTechs] = useState('');
+  const [price, setPrice] = useState('');
+
+  function handleSubmit(){
+
+  }
+ 
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <label id="thumbnail">
+        <input type="file"  />
+        <img src={camera} alt=""/>
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input type="text"
+        id="company"
+        placeholder="Sua empresa incrivel"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="company">TECNOLOGIAS * <span>Separar por vírgula</span> </label>
+      <input type="text"
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="company">VALOR DA DIÁRIA <span> Em caso de gratuito, deixar em branco</span> </label>
+      <input type="text"
+        id="price"
+        placeholder="R$0,00"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">Cadastrar</button>
+    </form>
+  )
 
 }
