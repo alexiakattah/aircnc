090479fbc7aab99dc07954b7bcf57947e078187a
Alexia Kattah <alexiakattah_97@hotmail.com>
2019-10-05 09:21:37 -0300

    socket.io backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log('usuario conectado', socket.id);
+
+  socket.emit('message', 'Conectado')
+})
 
 mongoose.connect('mongodb+srv://alexiakattah:1997Mouse@cluster0-45sp1.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser : true,
@@ -17,4 +27,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..','uploads')))
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
